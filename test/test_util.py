6453d54ac8c9327ac7ab66b17d49b61b96c7a805
Jenek Basota <jenekbasota@MacBook-Air-Jenek.local>
2024-12-14 20:17:58 +0300

    test

---
+++ test/test_util.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+def test_upper():
+    assert 'foo'.upper() == 'FOO'
+    
+def test_isupper():
+    assert 'FOO'.isupper()
+    
+def test_failed_upper():
+    assert 'foo'.upper() == 'FOO'
