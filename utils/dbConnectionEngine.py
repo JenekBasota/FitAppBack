a800a30ec2a9c70c6f337fd1e0a23e3ae1f7081a
Jenek Basota <jenekbasota@MacBook-Air-Jenek.local>
2024-12-15 00:29:16 +0300

    test to run tests8

--- utils/dbConnectionEngine.py
+++ utils/dbConnectionEngine.py
@@ -7,5 +7,5 @@ class dbConnectionEngine:
         url = os.getenv('DATABASE_URL')  # или другой способ получения строки подключения
         if not url:
          raise ValueError("DATABASE_URL is not set or is invalid")
-        engine = create_engine(url, pool_size=50, echo=False)
+        engine = create_engine("postgresql://postgres:0180@localhost:5432/FitnessAppDB", pool_size=50, echo=False)
         return engine
