468dedb589968abaef44f92bc9b53ddd9927ee51
Jenek Basota <jenekbasota@MacBook-Air-Jenek.local>
2024-12-14 23:58:22 +0300

    test to run tests6

--- utils/dbConnectionEngine.py
+++ utils/dbConnectionEngine.py
@@ -3,7 +3,9 @@ import os
 
 
 class dbConnectionEngine:
-    def get_engine(self):
-        url = os.getenv("URL")
+   def get_engine(self):
+        url = os.getenv('URL')  # или другой способ получения строки подключения
+        if not url:
+         raise ValueError("DATABASE_URL is not set or is invalid")
         engine = create_engine(url, pool_size=50, echo=False)
         return engine
